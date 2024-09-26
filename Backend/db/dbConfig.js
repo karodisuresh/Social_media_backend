ef84e925a55ef0f7dbe781ef74c3e81f67b5bcab
karodisuresh <sureshreddykarodhi@gmail.com>
2024-09-26 07:26:52 +0530

    new version updated

--- Backend/db/dbConfig.js
+++ Backend/db/dbConfig.js
@@ -1,18 +1,17 @@
-const express = require('express');
 const mongoose =require('mongoose');
 
 async function connectMongoDB(){
 try{
-    let mongoURI
+    let mongoURI;
     if (process.env.NODE_ENV === 'production') {
         mongoURI = process.env.MONGODB_URI_PROD;
         console.log(mongoURI)
     } else {
-        mongoURI = process.env.MONGODB_URI_PROD;
+        mongoURI = process.env.MONGODB_URI_DEV;
     }
     await mongoose.connect(mongoURI,{
-        // useNewParsers: true,
-        // useUndefinedTopology: true
+        useNewUrlParser: true,
+        useUnifiedTopology: true
     })
     console.log('connect success to mongodb')
 }catch(err){
